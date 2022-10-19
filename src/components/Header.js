459379b77c1473c864949deeeb25306c5d4ce2a2
Sajal1005 <nandasajal.208@gmail.com>
2022-10-19 10:39:37 +0530

    before adding redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 import { MenuIcon, SearchIcon,ShoppingCartIcon } from "@heroicons/react/outline"
 import { signIn, signOut ,useSession} from "next-auth/react";
+import { useRouter } from 'next/router';
 
 function Header() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   return (
     <header>
@@ -12,7 +14,9 @@ function Header() {
     {/* Top Nav */}
     <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
         <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-            <Image src='https://links.papareact.com/f90' width={150} height={40} objectFit="contain"
+            <Image 
+            onClick={() => router.push('/')} 
+            src='https://links.papareact.com/f90' width={150} height={40} objectFit="contain"
             className='cursor-pointer' />
         </div>
 
@@ -34,7 +38,7 @@ function Header() {
             <p>Returns</p>
             <p className='font-extrabold md:text-sm'>& Orders</p>
           </div>
-          <div className='link relative flex items-center   '>
+          <div onClick={()=> router.push("/checkout")} className='link relative flex items-center   '>
             <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
             <ShoppingCartIcon className='h-10'/>
             <p className='font-extrabold hidden md:text-sm md:inline mt-2'>Basket</p>
--- src/components/Product.js
+++ src/components/Product.js
@@ -34,13 +34,6 @@ function Product({id,title,price,description,category,image}) {
       <div className='mb-5'>
             <p>${price}</p>
       </div>
-      
-      {/* {hasPrime ? (
-        <div className='flex items-center space-x-2 -mt-5'>
-          <img className='w-12' src="https://links.papareact.com/fdw" alt="" />
-          <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
-        </div>
-      ):(null)} */}
 
       {hasPrime && (
         <div className='flex items-center space-x-2 -mt-5'>
@@ -49,7 +42,7 @@ function Product({id,title,price,description,category,image}) {
         </div>
       )}
 
-      <button className='mt-auto button'>Add to Basket</button>
+      <button onClick={addItemToBasket} className='mt-auto button'>Add to Basket</button>
 
     </div>
   )
