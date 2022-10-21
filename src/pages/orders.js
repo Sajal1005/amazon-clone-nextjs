559d8de21eed7816c431b515e9d3a0a47609a8f1
Sajal1005 <nandasajal.208@gmail.com>
2022-10-21 18:10:11 +0530

    completed

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -3,24 +3,29 @@ import Header from '../components/Header'
 import { getSession, useSession } from 'next-auth/react';
 import moment from "moment";
 import db from "../../firebase"
+import Order from '../components/Order';
 
 function Orders({orders}) {
   const { data: session } = useSession();
 
-  console.log(orders)
   return (
     <div>
       <Header />
       <main className='max-w-screen-lg mx-auto p-10'>
         <h1 className='text-3xl border-b mb-2 pb-1 border-yellow-400'>Your Orders</h1>
         {session ? (
-          <h2>x Orders</h2>
+          <h2>{orders.length} Orders</h2>
         ) : (
           <h2>Please sign in to see your orders</h2>
         )}
 
         <div className='mt-5 space-y-4'>
-
+          {orders?.map(
+            ({id, amount, amountShipping, items, timestamp, images}) => (
+            <Order 
+            key={id} id={id} amount={amount} amountShipping={amountShipping} items={items} timestamp={timestamp} images={images}
+            />
+          ))}
         </div>
       </main>
     </div>
@@ -53,7 +58,7 @@ export async function getServerSideProps(context){
       timestamp: moment(order.data().timestamp.toDate()).unix(),
       items: (
         await stripe.checkout.sessions.listLineItems(order.id, {
-          limits: 100,
+          limit: 100,
         })
       ).data,
     }))
